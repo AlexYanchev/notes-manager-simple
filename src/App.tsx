2a6c5cc3398c92656a4bf132a0e657a1c1faeb2b
Alexey Yanchev <yanchevalexey@yandex.ru>
2025-02-01 15:56:32 +0300

    Добавил провайдеры и NotesManager в App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import NotesManager from './components/NotesManager/NotesManager';
+import { Provider } from 'react-redux';
+import { storeNotesManager } from './redux/store';
+import StorageProvider from './contexts/StorageProvider';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <Provider store={storeNotesManager}>
+      <StorageProvider>
+        <DndProvider backend={HTML5Backend}>
+          <NotesManager />
+        </DndProvider>
+      </StorageProvider>
+    </Provider>
+  );
 }
 
 export default App;
